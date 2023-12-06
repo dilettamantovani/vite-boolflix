32b42051e5c8182774553630db3152954f6410f4
dilettits <mantovani.d3@gmail.com>
2023-12-06 12:45:11 +0100

    added poster, lang flag, stars

--- src/components/SeriesCard.vue
+++ src/components/SeriesCard.vue
@@ -4,20 +4,45 @@ export default {
     props: {
         series: Object
     },
+
+    methods: {
+        getFlag(lang) {
+            return new URL(`../assets/flags/${lang}.png`, import.meta.url).href
+        },
+
+        getVote(vote) {
+            return Math.ceil(vote / 2)
+        },
+    },
+
 }
 </script>
 
 <template>
     <div class="card" id="seriesCard">
+        <img class="poster" :src="`https://image.tmdb.org/t/p/w342/${series.poster_path}`" alt="">
         <h3>{{ series.name }}</h3>
         <h6><em>(Original Title: {{ series.original_name }})</em></h6>
-        <h6><em>(Original Language: {{ series.original_language }})</em></h6>
-        <h4>{{ series.popularity }}</h4>
+        <p><em>(Original Language: <img class="flag" :src="getFlag(series.original_language)" alt="">)</em></p>
+        <i v-for="i in 5" :class="{ 'filling': i <= getVote(series.vote_average) }" class="fa-solid fa-star"></i>
     </div>
 </template>
 
 <style scoped>
 #seriesCard {
     background-color: blueviolet;
+}
+
+.poster {
+    width: 13.7rem;
+    padding: 0.5rem;
+}
+
+.flag {
+    width: 1.5rem;
+}
+
+.filling {
+    color: yellow;
 }
 </style>
